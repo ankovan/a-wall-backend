3ae338d3d344dc6e9101bf95ff3342dde50f6453
ankovan <personindifferent@gmail.com>
2021-10-28 01:42:04 +0300

    bugfix

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
@@ -27,3 +26,4 @@ express()
       res.send("Error " + err);
     }
   })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
