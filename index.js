44d7ad2e8497b0c0c8db151faee2dc6c04e7ea25
ankovan <personindifferent@gmail.com>
2021-11-06 15:25:05 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const pool = new Pool({
   }
 });
 const express = require('express')
+const cors = require('cors')
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(cors({origin: "*"}))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
